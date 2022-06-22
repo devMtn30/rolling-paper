84fe88000c9b0a6330eb15b76ef31e0c461bf112
devMtn30 <devMtn30@gmail.com>
2022-06-22 21:28:53 +0900

    db 업데이트

--- src/main/java/com/memorymakerpeople/memoryrollingpaper/dto/CardResponseDto.java
+++ src/main/java/com/memorymakerpeople/memoryrollingpaper/dto/CardResponseDto.java
@@ -2,10 +2,12 @@ package com.memorymakerpeople.memoryrollingpaper.dto;
 
 import com.memorymakerpeople.memoryrollingpaper.domain.Card;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.sql.Timestamp;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class CardResponseDto extends DefaultResponseDto{
     private int cardId;
